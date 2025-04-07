8417abadea36f62255bb170683a494ca46d35ff6
Christof Thyssen <christofthy@gmail.com>
2025-04-07 21:42:00 +0200

    Platform independent path and filename safe characters

--- Acropolis/Acropolis.Infrastructure/Helpers/PathHelpers.cs
+++ Acropolis/Acropolis.Infrastructure/Helpers/PathHelpers.cs
@@ -1,10 +1,20 @@
 ﻿using System.Text;
 
 namespace Acropolis.Infrastructure.Helpers;
+
 public static class PathHelpers
 {
-    public static char[] InvalidFileNameCharacters { get; } = Path.GetInvalidFileNameChars();
-    public static char[] InvalidPathCharacters { get; } = Path.GetInvalidPathChars();
+    public static char[] InvalidFileNameCharacters { get; } =
+    [
+        '\'', '<', '>', '|', '\u0000', '\u0001', '\u0002', '\u0003', '\u0004', '\u0005', '\u0006', '\u0007', '\b', '\t', '\n', '\u000b', '\f', '\r', '\u000e', '\u000f', '\u0010', '\u0011', '\u0012',
+        '\u0013', '\u0014', '\u0015', '\u0016', '\u0017', '\u0018', '\u0019', '\u001a', '\u001b', '\u001c', '\u001d', '\u001e', '\u001f', ':', '*', '?', '\\', '/'
+    ];
+
+    public static char[] InvalidPathCharacters { get; } =
+    [
+        '|', '\u0000', '\u0001', '\u0002', '\u0003', '\u0004', '\u0005', '\u0006', '\u0007', '\b', '\t', '\n', '\u000b', '\f', '\r', '\u000e', '\u000f', '\u0010', '\u0011', '\u0012', '\u0013',
+        '\u0014', '\u0015', '\u0016', '\u0017', '\u0018', '\u0019', '\u001a', '\u001b', '\u001c', '\u001d', '\u001e', '\u001f'
+    ];
 
     public static string RemoveInvalidFileNameChars(this string filename)
     {
@@ -14,6 +24,7 @@ public static class PathHelpers
 
             sb.Replace(character.ToString(), "");
         }
+
         return sb.ToString();
     }
 
@@ -24,6 +35,7 @@ public static class PathHelpers
         {
             sb.Replace(character.ToString(), "");
         }
+
         return sb.ToString();
     }
 
